17f50f3105a737d752c0d01ff30fa55f7b2d0b98
Geezar <nirozaz@yahoo.com>
2021-04-26 17:01:45 +0300

    set a buttons

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,24 +4,34 @@ import { IoIosArrowBack } from 'react-icons/io'
 import { history } from '../redux'
 
 function Navbar() {
-  const { username } = useParams('')
+  const { username, reponame } = useParams('')
   return (
     <div className="max-w-screen-xl m-auto flex items-center justify-between p-4 border-b-4">
-      <div className="flex items-center justify-between p-4">
-        <button
-          type="button"
-          onClick={() => history.push('/')}
-          className="text-red-500 hover:underline hover:text-green-500 font-bold focus:outline-none"
-        >
+      <div className=" items-center justify-between p-4">
+        <button type="button" onClick={() => history.push('/')} className=" ">
           <div>
             {username && (
-              <div className="flex">
+              <div className="flex text-red-500 hover:text-green-500 font-bold focus:outline-none">
                 <IoIosArrowBack className="mt-1" />
                 To Search
               </div>
             )}
           </div>
         </button>
+        <br />
+        <div>
+          {reponame && (
+            <div
+              className="flex  hover:text-green-500 font-bold focus:outline-none cursor-pointer"
+              onClick={() => history.push(`/${username}`)}
+              onKeyDown={() => history.push(`/${username}`)}
+              aria-hidden="true"
+            >
+              <IoIosArrowBack className="mt-1" />
+              To Repositories
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
